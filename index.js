71a66937231f9579a16f27f9ecc4f00093489fd6
Michael Okello <okellofrost@gmail.com>
2022-07-08 11:39:51 +0300

    clean code

--- index.js
+++ index.js
@@ -1,21 +1,19 @@
 //exercise 4
-checkspeed(80);
+checkspeed(130);
 function checkspeed(speed) {
   const speedLimit = 70;
   const kmPerPoint = 5;
 
-  if (speed < speedLimit + kmPerPoint) {
+  if (speed < speedLimit + kmPerPoint) 
     console.log('Ok');
-  }
+  
   else {
     const points = Math.floor((speed - speedLimit) / kmPerPoint);
-    if (points >= 12) {
+    if (points >= 12) 
       console.log('Licence Suspended');
-
-    } else {
+   
+    else 
       console.log('Points ', points);
 
-    }
   }
-
 }
