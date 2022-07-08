6e8299748c4216e871d2cd0aeb55b611bca04604
Michael Okello <okellofrost@gmail.com>
2022-07-08 10:46:51 +0300

    Adding The Actual Code

---
+++ index.js
@@ -0,0 +1,21 @@
+//exercise 4
+checkspeed(80);
+function checkspeed(speed){
+const speedLimit = 70;
+const kmPerPoint = 5;
+
+if(speed <= speedLimit){
+  console.log('Ok');
+}
+else{
+  const points = Math.floor((speed - speedLimit) / kmPerPoint);
+  if(points >= 12) {
+      console.log('Licence Suspended');
+      
+  }else {
+       console.log('Points ' , points);
+
+      }
+}
+ 
+}
